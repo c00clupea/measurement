862af233a88dec41e43ea8f2a323762a0bcf3e5a
c00clupea <c00clupea@gmail.com>
2015-01-07 17:21:06 +0100

    +some scripts

---
+++ deploy_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+export CPATH=/usr/local/include
+export LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH=/usr/local/lib
+
+cd /home/c00clupea
+
+sudo rm -r c00clupeaperf-01
+
+tar -xzvf c00clupeaperf-01.tar.gz
+
+#tar -xzvf htdocs.tar.gz -C /var/c00clupea/
+
+cd c00clupeaperf-01
+
+./configure --enable-perfwithmain
+
+make -j3
+
+sudo make install
